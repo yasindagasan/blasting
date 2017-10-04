8155dbfd0ac5b1f245318b0ddd32880a5626fc93
Yasin Dagasan <yasin_dagasan@hotmail.com>
2017-10-04 21:50:13 +0800

    Add files via upload

---
+++ Functions/attenuationCalculate.R
@@ -0,0 +1,35 @@
+attenuationCalculate <- function(records){
+  
+  logPPVCalc <- log10(records$PPV) # an example to an arithmetic operation in R
+  logSDCalc  <- log10(records$SD)
+  
+  minPPVlog <- min(logPPVCalc)
+  maxPPVlog <- max(logPPVCalc)  
+  minSDlog  <- min(logSDCalc)
+  maxSDlog  <- max(logSDCalc)
+  
+  bestFitLineCalc <- lm(logPPVCalc~logSDCalc)
+  
+  
+  
+  kIntercept  <- 10^coefficients(bestFitLineCalc)[1]
+  slope       <- coefficients(bestFitLineCalc)[2]
+  sumBestFit  <- summary(bestFitLineCalc)
+  sigmBestFit <- sumBestFit$sigma
+  Kconf95     <- 10^(sigmBestFit*1.645+coefficients(bestFitLineCalc)[1])
+  
+  return(list(logPPV      = logPPVCalc,
+              logSD       = logSDCalc,
+              PPV         = records$PPV,
+              SD          = records$SD,
+              bestFitLine = bestFitLineCalc,
+              Intercept   = kIntercept,
+              Intercept95 = Kconf95,
+              minPPV      = minPPVlog,
+              maxPPV      = maxPPVlog,
+              minSD       = minSDlog,
+              maxSD       = maxSDlog,
+              slope       = slope)
+         )
+  
+}
---
+++ main.R
@@ -0,0 +1,25 @@
+#####################################################################
+# Project to use the boostrap confidence line in vibration prediction
+#####################################################################
+source("Functions/attenuationCalculate.R")
+source("Functions/plotAttenuation.R")
+
+# Import the data
+vibrationRecords <- read.csv("Data/vibration records.csv", header = T)
+
+# have a look at the structure of the data and the variables imported
+str(vibrationRecords)
+
+# view an individual variable
+vibrationRecords$PPV # view PPV data
+vibrationRecords$SD  # view SD data
+
+attenuation <- attenuationCalculate(vibrationRecords) # calculate the attenuation for the records named "vibrationRecords"
+
+# plot the attenuation calculated
+plotAttenuation(attenuation, # file with the attenuation calculations
+                rangeX =c(20,100), # limits of the plot in x 
+                rangeY = c(0.4,20) # limits of the plot in Y
+
+                )
+attenuation # get the statistics about the attenuation of the vibrations
