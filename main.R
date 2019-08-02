7563f53c4e914632139989881dea607672d85a3b
dagasany <yasin_dagasan@hotmail.com>
2019-08-03 00:01:35 +0200

    add initial Monte Carlo simulator

--- main.R
+++ main.R
@@ -1,30 +1,34 @@
 #####################################################################
 # Scripts to predict the blast induced ground vibrations
 #####################################################################
-source("Functions/attenuationCalculate.R")
-source("Functions/plotAttenuation.R")
+source("utils/attenuationCalculate.R")
+source("utils/plotAttenuation.R")
 
 # Import the data
-vibrationRecords <- read.csv("Data/vibration records.csv", header = T)
+vibrationRecords <- read.csv("dat/vibration records.csv", header = T)
 
 # have a look at the structure of the data and the variables imported
 str(vibrationRecords)
 
 # view an individual variable
-vibrationRecords$PPV # view PPV data
+vibrationRecords$PPV # view PPV data  
 vibrationRecords$SD  # view SD data
 
 # calculate the attenuation for the records named "vibrationRecords"
-attenuation <- attenuationCalculate(vibrationRecords) 
+attenuation <- attenuationCalculate(subset(vibrationRecords,Type %in% c("Test"))) 
+
+# plot the attenuation 
+plotAttenuation(attenuation, # file with the attenuation calculations
+                rangeX =c(20,100), # limits of the plot in x 
+                rangeY = c(0.4,20) # limits of the plot in Y
+                )
+
+# ---Monte Carlo Simulations (under development)---
+simulated_coef <- t(replicate(1000,mc_sim(attenuation)))
 
 # plot the attenuation calculated
 plotAttenuation(attenuation, # file with the attenuation calculations
                 rangeX =c(20,100), # limits of the plot in x 
-                rangeY = c(0.4,20) # limits of the plot in Y
-
-                )
-# get the statistics about the attenuation of the vibrations
-attenuation 
-
-
-
+                rangeY = c(0.4,20), # limits of the plot in Y
+                mc_samples = simulated_coef
+)
